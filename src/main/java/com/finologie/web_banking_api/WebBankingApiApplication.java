bbddf5ce47ec937d9e63ad367a128f2c8808d85b
bacem <bcem.kratos@gmail.com>
2024-07-13 15:53:53 +0200

    git init

---
+++ src/main/java/com/finologie/web_banking_api/WebBankingApiApplication.java
@@ -0,0 +1,13 @@
+package com.finologie.web_banking_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBankingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBankingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finologie/web_banking_api/WebBankingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finologie.web_banking_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBankingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
